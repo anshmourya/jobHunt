e8f5260489363a833b37e64a884eae6a47d8f7c1
anshmourya <anshmourya657@gmail.com>
2025-06-12 22:30:01 +0530

    fix

--- client/app/login/page.tsx
+++ client/app/login/page.tsx
@@ -31,6 +31,7 @@ export default function LoginPage() {
           routing="path"
           path="/login"
           signUpUrl="/signup"
+          forceRedirectUrl="/jobs"
         />
       </div>
     </div>
